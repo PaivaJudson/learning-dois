d2a35726df1dca01172085dd1e7fb190f7ad754e
judsonpaiva <judson.paiva@hotmail.com>
2022-11-13 00:18:17 +0100

    estilos em react commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
 import './App.css';
-import Corpo from './componentes/Corpo';
-import Header from './componentes/Header';
-
 
 function App() {
 
+  const textoDestaque = {
+    color: '#00f',
+    fontSize: '3em'
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Corpo />
-     
-    </div>
+    <>
+      <div className='App'>
+        <h1 style={{color:'#f00', fontSize:'5em'}}>App</h1>
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className='texto'>Se inscreva em nossos cursos online</p>
+        <a href="www.google.com" target='_blank' className>Link</a>
+      </div>
+    </>
   );
 }
 
